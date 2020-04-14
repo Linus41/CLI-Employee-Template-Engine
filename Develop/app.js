67468d52a720ed59830ea9530e07e4955b21020f
Linus41 <schief.craft@gmail.com>
2020-04-13 19:24:24 -0700

    add classes, constructors, and module.exports to engineer, intern, and manager files

--- Develop/app.js
+++ Develop/app.js
@@ -4,30 +4,57 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+const teamMembers = [];
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+inquirer 
+    .prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "managername"
+        },
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "name"
+        },
+
+    ]).then(response => {
+        console.log(response);
+        console.log(response.managername);
+        const managerInfo = new Manager(response.managername, response.email, etc)
+        // logs managerInfo object
+        console.log(managerInfo);
+        // building team members
+        teamMembers.push(managerInfo);
+        
+    })
+// make inquirer for each type of team member and make new object for each, push to global array
+function engineerDetails() {
+    inquirer
+        .prompt
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above to target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,34 @@
 // TODO: Write code to define and export the Employee class
+var info = require("../app")
+
+class Employee {
+    constructor(name, email, id) {
+        if(!name) {
+            throw new Error("You are missing the name.");
+          }
+          if (!email) {
+            throw new Error("You are missing your email.");
+          }
+          if (!id) {
+            throw new Error("You are missing your ID.");
+          }
+          this.name = name;
+          this.email = email;
+          this.id = id;
+    }
+    getName() {
+        return this.name;
+    }
+
+    getRole() {
+        return "Employee";
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+}
+
+module.exports = Employee
